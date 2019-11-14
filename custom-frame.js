f83e8f62a91fd75ed075687af0296db8102a6ea3
tomer-openfin <47791895+tomer-openfin@users.noreply.github.com>
2019-11-14 15:04:41 -0500

    Update custom-frame.js

--- custom-frame.js
+++ custom-frame.js
@@ -17,7 +17,7 @@ const setupFrameButtons = () => {
 const setupLayout = () => {
     fin.Window.getCurrentSync().getOptions().then(({layoutConfig}) => {
         let layoutContainer = document.getElementById('layout-container');
-        var layout = new Layout(layoutConfig, layoutContainer);
+        var layout = fin.Layout.create(layoutConfig, layoutContainer);
         layout.on('tabCreated', onTabCreated);
         layout.init();
     });
