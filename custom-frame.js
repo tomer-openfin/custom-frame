3639ca010fc47d5857f8c1972cc17ffdcb4d44ba
tomer-openfin <47791895+tomer-openfin@users.noreply.github.com>
2020-02-20 17:22:04 -0500

    Update custom-frame.js

--- custom-frame.js
+++ custom-frame.js
@@ -1,11 +1,7 @@
 win = fin.Window.getCurrentSync();
-win.on('layout-ready', payload => console.log('layout ready!   ' + JSON.stringify(payload) + '    ' + new Date().getTime()));
-win.on('layout-initialized', payload => console.log('layout initialized!   ' + JSON.stringify(payload) + '    ' + new Date().getTime()));
-
-fin.Application.getCurrentSync().on('window-layout-ready', payload => console.log('app: layout ready!   ' + JSON.stringify(payload) + '    ' + new Date().getTime()));
-fin.Application.getCurrentSync().on('window-layout-initialized', payload => console.log('app: layout initialized!   ' + JSON.stringify(payload) + '    ' + new Date().getTime()));
-fin.System.on('window-layout-ready', payload => console.log('System: layout ready!   ' + JSON.stringify(payload) + '    ' + new Date().getTime()));
-fin.System.on('window-layout-initialized', payload => console.log('System: layout initialized!   ' + JSON.stringify(payload) + '    ' + new Date().getTime()));
+p = fin.Platform.getCurrentSync();
+p.on('platform-snapshot-applied', () => console.log('snapshot applied'));
+p.on('platform-api-ready', () => console.log('api ready'));
 
 const setupFrameButtons = () => {
     const win = fin.Window.getCurrentSync();
